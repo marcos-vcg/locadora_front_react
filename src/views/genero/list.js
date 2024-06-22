317b7a04fdc45166c2eb9fdc40b5f703a8ae328e
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2024-06-22 01:22:12 -0300

    Update list.js

--- src/views/genero/list.js
+++ src/views/genero/list.js
@@ -9,90 +9,90 @@ import { ServiceGenero } from "../../services/genero";
 
 
 export default function GeneroList() {
-    const [entities, setEntities] = useState([]);
-    const navigate = useNavigate();
+  const [entities, setEntities] = useState([]);
+  const navigate = useNavigate();
 
 
-    useEffect(() => {
-        carregaLista();
-    }, []);
-
-
-
-    const columns = [
-        {
-            title: 'ID',
-            dataIndex: 'id',
-            key: 'id',
-            align: 'center',
-
-        },
-        {
-            title: 'Nome',
-            dataIndex: 'nome',
-            key: 'nome',
-            align: 'center',
-        },
-        {
-            title: 'Ações',
-            key: 'acao',
-            render: (text, record) => (
-                <Space size="middle">
-                    <Button>
-                        <Popconfirm
-                            title={"Deseja mesmo excluir?"}
-                            onConfirm={() => { deletar(record.id) }}
-                        >
-                            Deletar
-                        </Popconfirm>
-                    </Button>
-                    <Button>
-                        <Link to={prepararEditar(record.id)}>Editar</Link>
-                    </Button>
-                </Space>
-            ),
-            align: 'center',
-        },
-    ];
+  useEffect(() => {
+    carregaLista();
+  }, []);
 
 
 
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      align: 'center',
 
-    return (
-        <Col span={24}>
-            <PageHeader
-                title={"Listas de Gener"}
-                onBack={() => navigate(CLIENT_URL + "/")}
-                extra={[
-                    <Button
-                        key="1"
-                        type="primary"
-                        onClick={() => navigate(CLIENT_URL + "/genero/form")}
-                    >
-                        Novo Genero
-                    </Button>
-                ]}
-            />
-            <TabelaAnt
-                className={"tabela"}
-                columns={columns}
-                dataSource={entities?.length ? entities : []}
-            />
-        </Col>
-    )
+    },
+    {
+      title: 'Nome',
+      dataIndex: 'nome',
+      key: 'nome',
+      align: 'center',
+    },
+    {
+      title: 'Ações',
+      key: 'acao',
+      render: (text, record) => (
+        <Space size="middle">
+          <Button>
+            <Popconfirm
+              title={"Deseja mesmo excluir?"}
+              onConfirm={() => { deletar(record.id) }}
+            >
+              Deletar
+            </Popconfirm>
+          </Button>
+          <Button>
+            <Link to={prepararEditar(record.id)}>Editar</Link>
+          </Button>
+        </Space>
+      ),
+      align: 'center',
+    },
+  ];
 
 
 
-    async function carregaLista() {
-        let response = await ServiceGenero.getTodos()
-        setEntities(response.data)
-    }
 
-    async function deletar(id) {
-        await ServiceGenero.deletar(id).then(() => carregaLista())
-    }
+  return (
+    <Col span={24}>
+      <PageHeader
+        title={"Lista de Generos"}
+        onBack={() => navigate(CLIENT_URL + "/")}
+        extra={[
+          <Button
+            key="1"
+            type="primary"
+            onClick={() => navigate(CLIENT_URL + "/genero/form")}
+          >
+            Novo Genero
+          </Button>
+        ]}
+      />
+      <TabelaAnt
+        className={"tabela"}
+        columns={columns}
+        dataSource={entities?.length ? entities : []}
+      />
+    </Col>
+  )
 
-    function prepararEditar(id) {
-        return CLIENT_URL + "/genero/form/" + id
-    }
+
+
+  async function carregaLista() {
+    let response = await ServiceGenero.getTodos()
+    setEntities(response.data)
+  }
+
+  async function deletar(id) {
+    await ServiceGenero.deletar(id).then(() => carregaLista())
+  }
+
+  function prepararEditar(id) {
+    return CLIENT_URL + "/genero/form/" + id
+  }
 }
