bea9cd0c5863f52766482477e1816fbfd9e7f3b2
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2024-07-10 00:50:39 -0300

    add resposivity

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 } from '@ant-design/icons';
 
 import { Layout, Menu } from 'antd';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import "./styles/app.css"
@@ -40,24 +40,71 @@ const items = [
   getItem('Configurações', 'menu5', <Link to={"/config"} ><SettingOutlined /></Link>),
 ];
 
+const MenuBar = ({ isCollapsed, setCollapsed, isSmallDevice }) => {
+  return (
+    isSmallDevice ? <NavBar isSmallDevice={isSmallDevice} /> :
+
+      <Sider collapsible collapsed={isCollapsed} onCollapse={(value) => setCollapsed(value)}>
+        {/* <div className="logo" >Locadora</div> */}
+        <NavBar />
+      </Sider>
 
 
+  )
+}
+
+const NavBar = ({ isSmallDevice }) => {
+  return (
+    <Menu
+      theme="dark"
+      defaultSelectedKeys={['']}
+      defaultOpenKeys={['menu2']}
+      // mode="inline"
+      mode={isSmallDevice ? "horizontal" : "vertical"}
+      items={items}
+      selectable={false} />
+  )
+}
 
 const App = () => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [isCollapsed, setCollapsed] = useState(true);
+  // const [currentWidth, setCurrentWidth] = useState('');
+  const [isSmallDevice, setSmallDevice] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      window.innerWidth < 800 ? setSmallDevice(true) : setSmallDevice(false)
+    };
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [])
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="logo" >Locadora</div>
-        <Menu theme="dark" defaultSelectedKeys={['']} defaultOpenKeys={['menu2']} mode="inline" items={items} selectable={false} />
-      </Sider>
+      {/* <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}> */}
+      {/* <div className="logo" >Locadora</div> */}
+      {/* <Menu
+        theme="dark"
+        defaultSelectedKeys={['']}
+        defaultOpenKeys={['menu2']}
+        // mode="inline"
+        mode={isSmallDevice ? "horizontal" : "vertical"}
+        items={items}
+        selectable={false} /> */}
+      {/* </Sider> */}
+      <MenuBar collapsed={isCollapsed} setCollapsed={setCollapsed} isSmallDevice={isSmallDevice} />
 
       <Layout className="site-layout">
-        <Header
-          // className="site-layout-background"
+        {/* <Header
+          className="site-layout-background"
           style={{ padding: 0 }}
-        />
+        /> */}
         <Content className="site-layout-background" style={{ margin: '16px 16px' }} >
           <div style={{ padding: 24, minHeight: 360, }} >
             <Rotas />{/* Aqui será renderizado o componente da tela atual */}
